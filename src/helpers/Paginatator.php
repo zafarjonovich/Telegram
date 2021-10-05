3505ffd8bf1814b710280053275460d469555a4d
zafarjonovich <adashaliyev50@gmail.com>
2021-10-05 10:54:02 +0300

    Fixed ubuntu bug

---
+++ src/helpers/Paginatator.php
@@ -0,0 +1,100 @@
+<?php
+
+
+namespace zafarjonovich\Telegram\helpers;
+
+
+use zafarjonovich\Telegram\Keyboard;
+
+class Paginatator
+{
+    private $keyboard;
+
+    private $pageIndexMask = false;
+
+    private $prevButtonText = 'Prev page';
+
+    private $nextButtonText = 'Next page';
+
+    private $callbackDataCreater;
+
+    private $minIndex;
+
+    private $currentIndex;
+
+    private $maxIndex;
+
+    public function __construct(Keyboard $keyboard,\Closure $callbackDataCreater,int $minIndex,int $currentIndex,int $maxIndex)
+    {
+        $this->keyboard = $keyboard;
+
+        $this->minIndex = $minIndex;
+
+        $this->currentIndex = $currentIndex;
+
+        $this->maxIndex = $maxIndex;
+
+        $this->callbackDataCreater = $callbackDataCreater;
+    }
+
+    /**
+     * @param $mask
+     * @return $this
+     */
+    public function showPageIndex($mask)
+    {
+        $this->pageIndexMask = $mask;
+        return $this;
+    }
+
+    public function setNavigationButtonTexts($prevButtonText,$nextButtonText)
+    {
+        if(!is_string($prevButtonText) or !is_string($nextButtonText))
+            throw new \Exception('Texts must be string format');
+
+        if(empty($prevButtonText) or empty($nextButtonText))
+            throw new \Exception('Texts must not be empty');
+
+        $this->prevButtonText = $prevButtonText;
+        $this->nextButtonText = $nextButtonText;
+    }
+
+    public function create()
+    {
+        $keyboard = $this->keyboard;
+
+        $callbackDataCreater = $this->callbackDataCreater;
+
+        $prevIndex = $this->currentIndex == $this->minIndex ? $this->maxIndex : $this->currentIndex - 1;
+
+        $nextIndex = $this->currentIndex == $this->maxIndex ? $this->minIndex : $this->currentIndex + 1;
+
+        $keyboard->addCallbackDataButton(
+            $this->prevButtonText,
+            $callbackDataCreater($prevIndex)
+        );
+
+        if($this->pageIndexMask){
+
+            $buttonText = strtr($this->pageIndexMask,[
+                'nextIndex' => $nextIndex + 1 ,
+                'prevIndex' => $prevIndex + 1 ,
+                'currentIndex' => $this->currentIndex + 1 ,
+                'minIndex' => $this->minIndex + 1 ,
+                'maxIndex' => $this->maxIndex + 1
+            ]);
+
+            $keyboard->addCallbackDataButton(
+                $buttonText,
+                '-'
+            );
+        }
+
+        $keyboard->addCallbackDataButton(
+            $this->nextButtonText,
+            $callbackDataCreater($nextIndex)
+        );
+
+        return $keyboard;
+    }
+}
